a4d59376e2e9596037db926f45487945f9589f46
Lucas <lucas.vadilho@gmail.com>
2018-10-09 12:52:57 -0300

    Arruma presentation error

--- lista3/1478.c
+++ lista3/1478.c
@@ -4,30 +4,33 @@ void imprimir(int m, int n, int matriz[m][n]){
     int i, j;
     for(i = 0; i < m; i++){
         for(j = 0; j < n; j++){
-            printf("%3d", matriz[i][j]);
+            if(j == 0) printf("%3d", matriz[i][j]);
+            else printf(" %3d", matriz[i][j]);
         }
         printf("\n");
     }
-    printf("\n");
 }
 
 void main(){
     int ordem;
     do{
         scanf("%d", &ordem);
-        int matriz[ordem][ordem];
+        if(ordem != 0){
+            int matriz[ordem][ordem];
 
-        int i, j, k;
-        for(i = 0; i < ordem; i++){
-            for(j = i; j < ordem; j++){
-                matriz[i][j] = j - i + 1;    
+            int i, j, k;
+            for(i = 0; i < ordem; i++){
+                for(j = i; j < ordem; j++){
+                    matriz[i][j] = j - i + 1;    
+                }
+
+                for(k = i; k < ordem; k++){
+                    matriz[k][i] = k - i + 1;
+                }
             }
 
-            for(k = i; k < ordem; k++){
-                matriz[k][i] = k - i + 1;
-            }
+            imprimir(ordem, ordem, matriz);
+            printf("\n");
         }
-
-        imprimir(ordem, ordem, matriz);
     }while(ordem != 0);
 }
